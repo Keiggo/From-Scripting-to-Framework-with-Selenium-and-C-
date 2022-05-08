c72d64d1380169498aff6058cc71864fd73348d4
Keiggo <darren.keig@gmail.com>
2022-05-08 07:06:35 +0100

    Chapter 2 completed. The tests won't run because of that fucking cookie popup which wasn't there when the original video was created. I can find it easily via CSS in dev tools but that doesn't then translate to VS code. Rather than waste a morning on this, I am moving on. Maybe Sarah can help later.

--- Royale.Tests/CardTests.cs
+++ Royale.Tests/CardTests.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Royale.Tests
 {
@@ -10,7 +12,7 @@ namespace Royale.Tests
         [SetUp]
         public void BeforeEach()
         {
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(Path.GetFullPath(@"../../../../" + "_drivers"));
         }
 
         [TearDown]
@@ -22,7 +24,37 @@ namespace Royale.Tests
         [Test]
         public void Ice_Spirit_is_on_Cards_Page()
         {
-            Assert.Pass();
+            // go to statsroyale.com
+            driver.Url = "https://statsroyale.com/";
+            driver.Manage().Window.Maximize();
+            // driver.FindElement(By.CssSelector("'button[title=Accept]'")).Click();
+            //click on cards link in header nav
+            driver.FindElement(By.CssSelector("a[href='/cards']")).Click();
+            //assert ice spirit is displayed
+            var iceSpirit = driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
+            Assert.That(iceSpirit.Displayed);
+        }
+
+        [Test]
+        public void Ice_Spirit_headers_are_correct_on_Card_Details_Page()
+        {
+            // go to statsroyale.com
+            driver.Url = "statsroyale.com";
+            //click on cards link in header nav
+            driver.FindElement(By.CssSelector("a[href='/cards']")).Click();
+            //go to Ice Spirit
+            driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']")).Click();
+            //Assert basic header stats
+            var cardName = driver.FindElement(By.CssSelector("[class*='cardName']")).Text;
+            var cardCategories = driver.FindElement(By.CssSelector(".card_rarity")).Text.Split(", ");
+            var cardType = cardCategories[0];
+            var cardArena = cardCategories[1];
+            var cardRarity = driver.FindElement(By.CssSelector(".card__common")).Text;
+            
+            Assert.AreEqual("Ice Spirit", cardName);
+            Assert.AreEqual("Troop",cardType);
+            Assert.AreEqual("Arena 8", cardArena);
+            Assert.AreEqual("Common", cardRarity);
         }
     }
 }
